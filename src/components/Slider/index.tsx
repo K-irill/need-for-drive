0eb35659f0999560091cbe572328d61c0a18cac3
Kirill <kirill.kondrashkin@inbox.ru>
2022-06-30 17:19:22 +0300

    add-slider

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,29 +1,37 @@
 import React from "react";
-import { ReactComponent as ArrowLeft } from "../../images/arrow_left.svg";
-import { ReactComponent as ArrowRight } from "../../images/arrow_right.svg";
 import "./Slider.scss";
+import { Navigation, Pagination, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
 
 const Slider = () => {
   return (
-    <div className="slider">
-      <div className="slider__btn-container">
-        <button className="slider__btn-left">
-          <ArrowLeft />
-        </button>
-        <button className="slider__btn-right">
-          <ArrowRight />
-        </button>
-      </div>
-      <div className="slide">
+    <Swiper
+      modules={[Navigation, Pagination, A11y]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+    >
+      <SwiperSlide>
         <img src={require("../../images/first-slide.jpg")} alt="first-slide" />
-        {/* <img
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
           src={require("../../images/second-slide.jpg")}
           alt="second-slide"
         />
+      </SwiperSlide>
+      <SwiperSlide>
         <img src={require("../../images/third-slide.jpg")} alt="third-slide" />
-        <img src={require("../../images/four-slide.jpg")} alt="four-slide" /> */}
-      </div>
-    </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={require("../../images/four-slide.jpg")} alt="four-slide" />
+      </SwiperSlide>
+    </Swiper>
   );
 };
 
