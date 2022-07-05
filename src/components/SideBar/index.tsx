e581c1908a3c3b8f0d76db4dde76bbf5fbe097ca
Kirill <kirill.kondrashkin@inbox.ru>
2022-07-05 21:50:17 +0300

    add-side-bar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { ReactComponent as MenuBtn } from "../../images/menu_btn.svg";
+import { ReactComponent as MenuBtnClose } from "../../images/menu_btn-close.svg";
+import { ReactComponent as Telegram } from "../../images/Telegram_white.svg";
 import "./SideBar.scss";
 
 const SideBar = () => {
@@ -8,19 +10,39 @@ const SideBar = () => {
   const openMenu = () => setIsOpen((prev) => !prev);
 
   return (
-    <div className="side-bar">
-      <div className="side-bar_open">
-        <div className="option-menu"></div>
-      </div>
-      <div className="side-bar_close">
-        <div className="side-bar__menu-btn">
-          <MenuBtn onClick={openMenu} />
+    <>
+      {isOpen ? (
+        <div className="side-bar_open">
+          <div className="option-menu">
+            <div className="option-menu__close-btn">
+              <MenuBtnClose onClick={openMenu} />
+            </div>
+            <div className="option-menu__content">
+              <span>ПАРКОВКА</span>
+              <span>СТРАХОВКА</span>
+              <span>БЕНЗИН</span>
+              <span>ОБСЛУЖИВАНИЕ</span>
+            </div>
+            <div className="option-menu__socials">
+              <Telegram />
+              <Telegram />
+              <Telegram />
+            </div>
+          </div>
         </div>
-        <div className="side-bar__switch-lang">
-          <span>Eng</span>
+      ) : (
+        <div className="side-bar">
+          <div className="side-bar_close">
+            <div className="side-bar__menu-btn">
+              <MenuBtn onClick={openMenu} />
+            </div>
+            <div className="side-bar__switch-lang">
+              <span>Eng</span>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -17,18 +17,53 @@ const Slider = () => {
       pagination={{ clickable: true }}
     >
       <SwiperSlide>
+        <div className="swiper__information">
+          <h2 className="information__header">Бесплатная парковка</h2>
+          <p className="information__content">
+            Оставляйте машину на платных городских парковках и разрешенных
+            местах, не нарушая ПДД, а также в аэропортах.
+          </p>
+          <button className="information__button">Подробнее</button>
+        </div>
         <img src={require("../../images/first-slide.jpg")} alt="first-slide" />
       </SwiperSlide>
       <SwiperSlide>
+        <div className="swiper__information">
+          <h2 className="information__header">Страховка</h2>
+          <p className="information__content">
+            Полная страховка страховка автомобиля
+          </p>
+          <button className="information__button information__button--second">
+            Подробнее
+          </button>
+        </div>
         <img
           src={require("../../images/second-slide.jpg")}
           alt="second-slide"
         />
       </SwiperSlide>
       <SwiperSlide>
+        <div className="swiper__information">
+          <h2 className="information__header">Бензин</h2>
+          <p className="information__content">
+            Полный бак на любой заправке города за наш счёт
+          </p>
+          <button className="information__button information__button--third">
+            Подробнее
+          </button>
+        </div>
         <img src={require("../../images/third-slide.jpg")} alt="third-slide" />
       </SwiperSlide>
       <SwiperSlide>
+        <div className="swiper__information">
+          <h2 className="information__header">Обслуживание</h2>
+          <p className="information__content">
+            Автомобиль проходит еженедельное ТО
+          </p>
+          <button className="information__button information__button--four">
+            Подробнее
+          </button>
+        </div>
         <img src={require("../../images/four-slide.jpg")} alt="four-slide" />
       </SwiperSlide>
     </Swiper>
