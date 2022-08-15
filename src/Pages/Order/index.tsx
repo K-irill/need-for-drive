c8637aab4b3dc9114a9aa877fded4c686f2204aa
Kirill Kondrashkin <kirill.kondrashkin@inbox.ru>
2022-08-15 11:09:14 +0400

    order-header

--- src/Pages/Order/index.tsx
+++ src/Pages/Order/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "./Order.scss";
+import Header from "../../components/Header";
+import SideBar from "../../components/SideBar";
 
 const Order = () => {
-  return <div>Order</div>;
+  return (
+    <div className="order">
+      <div className="order__side-bar">
+        <SideBar />
+      </div>
+      <div className="order__content">
+        <div className="content-header">
+          <Header />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Order;
